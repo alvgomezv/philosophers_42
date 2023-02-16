85e0dd667dfb2c20a6f3b63336729148de5505bb
alvgomezv <alkitegv@gmail.com>
2023-02-16 19:38:50 +0100

    Creo rutina de threads y comienzo con los stages
    Creo archivo stages.c
    Creo el stage eating
    Creo la rutina de los threads
    Añado el struct a la rutina

--- sources/philo.c
+++ sources/philo.c
@@ -6,11 +6,16 @@
 /*   By: alvgomez <alvgomez@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/16 10:28:34 by alvgomez          #+#    #+#             */
-/*   Updated: 2023/02/16 13:20:06 by alvgomez         ###   ########.fr       */
+/*   Updated: 2023/02/16 19:38:08 by alvgomez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/philo.h"
+
+void	leaks(void)
+{
+	system("leaks -q philo");
+}
 
 int	ft_atoi(char *str)
 {
@@ -42,11 +47,11 @@ t_philo	*inicialize_philo(char **argv)
 	p = (t_philo *)malloc(sizeof(t_philo));
 	if (!p)
 		perror("Failed to allocate memory");
-	p->nbr_philo = ft_atoi(argv[1]);
-	if (p->nbr_philo <= 0)
+	p->nb_philo = ft_atoi(argv[1]);
+	if (p->nb_philo <= 0)
 		perror("Incorrect argument");
-	p->philos = (pthread_t *)malloc(p->nbr_philo * sizeof(pthread_t));
-	if (!p->philos)
+	p->threads = (pthread_t *)malloc(p->nb_philo * sizeof(pthread_t));
+	if (!p->threads)
 		perror("Failed to allocate memory");
 	p->time_to_die = ft_atoi(argv[2]);
 	p->time_to_eat = ft_atoi(argv[3]);
@@ -55,36 +60,66 @@ t_philo	*inicialize_philo(char **argv)
 		perror("Incorrect argument");
 	if (argv[5])
 	{
-		p->nbr_must_eat = ft_atoi(argv[5]);
-		if (p->nbr_must_eat <= 0)
+		p->nb_must_eat = ft_atoi(argv[5]);
+		if (p->nb_must_eat <= 0)
 			perror("Incorrect argument");
 	}
 	return (p);
 }
 
-void	*routine(void *a)
+void eating(t_philo	*p)
 {
+	struct timeval tv;
+	unsigned long long 	milliseconds;
+	
+	gettimeofday(&tv, NULL);
+	milliseconds = (tv.tv_sec) * 1000 + (tv.tv_usec) / 1000;
+	printf("%d %d is eating\n", tv.tv_usec, p->philo);
+}
+
+void	*routine(void *arg)
+{
+	t_philo	*p;
+
+	p = (t_philo *)arg;
+	if(pthread_mutex_lock(&p->mutex) != 0)
+		perror(strerror(errno));
+	eating(p);
+	if(pthread_mutex_unlock(&p->mutex) != 0)
+		perror(strerror(errno));
 	return (0);
 }
 
 void	philo_routine(t_philo *p)
 {
-	int	i;
-
+	int				i;
+	t_philo			**aux;
+	
 	i = 0;
-	while (i < p->nbr_philo)
+	//aux = (t_philo **)malloc(p->forks * sizeof(t_philo *));
+	//if (!aux)
+	//	perror("Failed to allocate memory");
+	pthread_mutex_init(&p->mutex, NULL);
+	while (i < p->nb_philo)
 	{
-		if (pthread_create(&p->philos[i], NULL, &routine, NULL) != 0)
+		//if (!aux[i])
+		//aux[i] = (t_philo *)malloc(sizeof(t_philo));
+		//	perror("Failed to allocate memory");
+		//*aux[i] = *p;
+		//aux[i]->philo = i + 1;
+		if (pthread_create(&p->threads[i], NULL, &routine, p) != 0)
 			perror("Failed to create thread");
 		i++;
 	}
 	i = 0;
-	while (i < p->nbr_philo)
+	while (i < p->nb_philo)
 	{
-		if (pthread_join(p->philos[i], NULL) != 0)
+		if (pthread_join(p->threads[i], NULL) != 0)
 			perror("Failed to create thread");
 		i++;
 	}
+	pthread_mutex_destroy(&p->mutex);
+	//free(aux);
 }
 
 int	main(int argc, char **argv)
@@ -96,7 +131,8 @@ int	main(int argc, char **argv)
 		perror("Incorrect number of arguments");
 	p = inicialize_philo(argv);
 	philo_routine(p);
-	free(p->philos);
+	free(p->threads);
 	free(p);
+	//atexit(leaks);
 	return (0);
 }
--- sources/stages.c
+++ sources/stages.c
@@ -1,37 +1,14 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   philo.h                                            :+:      :+:    :+:   */
+/*   stages.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: alvgomez <alvgomez@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/02/16 10:41:28 by alvgomez          #+#    #+#             */
-/*   Updated: 2023/02/16 19:37:27 by alvgomez         ###   ########.fr       */
+/*   Created: 2023/02/16 15:31:31 by alvgomez          #+#    #+#             */
+/*   Updated: 2023/02/16 15:48:32 by alvgomez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PHILO_H
-# define PHILO_H
-
-# include <stdio.h>
-# include <unistd.h>
-# include <stdlib.h>
-# include <string.h>
-# include <pthread.h>
-#include <sys/time.h>
-#include <sys/errno.h>
-
-typedef struct s_philo
-{
-	pthread_t		*threads;
-	pthread_mutex_t mutex;
-	int				philo;
-	int				nb_philo;
-	int				time_to_die;
-	int				time_to_eat;
-	int				time_to_sleep;
-	int				nb_must_eat;
-}				t_philo;
-
-#endif
+#include "../includes/philo.h"
 
